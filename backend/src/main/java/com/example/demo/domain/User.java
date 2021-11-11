e1142a3d39a50cff11fd8abad66229aa3856d836
rhyuys115 <ryeongseok1125@gmail.com>
2021-11-11 19:07:54 +0900

    feat: User 클래스 추가

---
+++ backend/src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,37 @@
+package com.example.demo.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @Builder
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
