bc9c2115ae69adaf4febc541e647cb910c89f78b
rhyuys115 <ryeongseok1125@gmail.com>
2021-11-11 19:11:31 +0900

    feat: Group 클래스 추가

---
+++ backend/src/main/java/com/example/demo/domain/Group.java
@@ -0,0 +1,21 @@
+package com.example.demo.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String groupName;
+}
