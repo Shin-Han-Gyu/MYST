da6405ff67b3160373b92f8183d879ce09ae5e30
HeeJung, Lee <irr2000@paran.com>
2021-11-18 04:36:33 +0900

    feat: Task API 병합

--- backend/src/main/java/com/example/demo/controller/TaskController.java
+++ backend/src/main/java/com/example/demo/controller/TaskController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.domain.User;
 import com.example.demo.dto.TaskReqDto;
 import com.example.demo.dto.TaskResDto;
+import com.example.demo.service.GroupTaskService;
 import com.example.demo.service.TaskService;
 import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +22,7 @@ import java.util.List;
 public class TaskController {
 
     private final TaskService taskService;
+    private final GroupTaskService groupTaskService;
 
     @ApiOperation(value = "todo 생성")
     @ApiImplicitParams({@ApiImplicitParam(name = "jwt", value = "JWT Token", required = true, dataType = "string", paramType = "header")})
@@ -28,6 +30,18 @@ public class TaskController {
     public ResponseEntity<String> createPersonalTask(@ApiIgnore final Authentication authentication, @RequestBody TaskReqDto reqDto) {
         if(!check_Auth(authentication)) return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         if(taskService.createPersonalTask(jwt_to_userId(authentication), reqDto.getTaskName())) {
+            return ResponseEntity.status(HttpStatus.CREATED).body("CREATED");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("FAIL");
+        }
+    }
+
+    @ApiOperation(value = "team todo 생성")
+    @ApiImplicitParams({@ApiImplicitParam(name = "jwt", value = "JWT Token", required = true, dataType = "string", paramType = "header")})
+    @PostMapping("/{teamId}")
+    public ResponseEntity<String> createGroupTask(@ApiIgnore final Authentication authentication, @PathVariable("teamId") Long groupId, @RequestBody TaskReqDto reqDto) {
+        if(!check_Auth(authentication)) return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        if(groupTaskService.createGroupTask(jwt_to_userId(authentication), groupId , reqDto)) {
             return ResponseEntity.status(HttpStatus.CREATED).body("CREATED");
         } else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("FAIL");
@@ -76,6 +90,17 @@ public class TaskController {
     public ResponseEntity<List<TaskResDto>> readTodo(@ApiIgnore final Authentication authentication) {
         if(!check_Auth(authentication)) return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         List<TaskResDto> ret = taskService.readTodo(jwt_to_userId(authentication));
+
+        if(ret == null) return  ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        else return ResponseEntity.status(HttpStatus.OK).body(ret);
+    }
+
+    @ApiOperation(value = "team todo 읽기")
+    @ApiImplicitParams({@ApiImplicitParam(name = "jwt", value = "JWT Token", required = true, dataType = "string", paramType = "header")})
+    @GetMapping("/{teamId}")
+    public ResponseEntity<List<TaskResDto>> readGroupTodo(@ApiIgnore final Authentication authentication, @PathVariable("teamId") Long groupId) {
+        if(!check_Auth(authentication)) return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        List<TaskResDto> ret = groupTaskService.readTodo(jwt_to_userId(authentication), groupId);
 
         if(ret == null) return  ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         else return ResponseEntity.status(HttpStatus.OK).body(ret);
