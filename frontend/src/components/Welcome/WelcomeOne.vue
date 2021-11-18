bbe0f6dd3390342925585546b8bb107582bb0166
김호석 <hoseok76081024@gmail.com>
2021-11-18 17:14:15 +0900

    feat : welcome page 추가

---
+++ frontend/src/components/Welcome/WelcomeOne.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="welcome1 d-flex flex-column justify-content-center">
+    <h1 class="p1"> {{title}} </h1>
+    <h2 class="p2"> {{subject}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  // name: "WelcomeOne",
+  props:['title','subject']
+}
+</script>
+
+<style>
+  .welcome1{
+    height: 1000px;
+    width: 100%;
+    padding-bottom: 200px;
+    background-color:floralwhite;
+    text-align: center;
+  }
+  .p1{
+    font-size: 60px;
+    font-weight: bold;
+    color:teal;
+  }
+
+  .p2{
+    font-weight: bold;
+    color: darkgray;
+  }
+</style>
--- frontend/src/views/EntrancePage.vue
+++ frontend/src/views/EntrancePage.vue
@@ -1,11 +1,20 @@
 <template>
-  <h1>no login -> entrancepage</h1>
+  <div style="form1">
+    <welcome-one title="개인알림장과 공유알림장을 동시에" subject="팀원과 공유를 하면서 나만의 알림장을 써봐요"></welcome-one>
+    <welcome-two title2="까먹지 않도록 메모하세요" subject2="초등학생보다 못한 내 기억력을위한 나만의 알림장"></welcome-two>
+    <welcome-one title="Work - Life - Balance" subject="회사일과 개인일을 구분하여, 워라벨을 체크하세요"></welcome-one>
+    <welcome-two title2="구분되는 회사일과 나의일" subject2="개인 할 일과 회사 일을 구분하여 헷갈리지 않게, 빠뜨리지 않게 하세요"></welcome-two>
+  </div>
 </template>
 
 <script>
+import WelcomeOne from "@/components/Welcome/WelcomeOne.vue"
+import WelcomeTwo from "@/components/Welcome/WelcomeTwo.vue"
 export default {
   name: "MainPage",
   components: {
+    "welcome-one" : WelcomeOne,
+    "welcome-two" : WelcomeTwo,
   },
   created() {
     if (this.$store.state.login.userinfo.username != null) {
@@ -16,5 +25,8 @@ export default {
 </script>
 
 <style>
-
+  .form1{
+    width: 100%;
+    height: 300px;
+  }
 </style>
