80cab18e5366b7cc9a8a6effa2909d01beaba6d8
HeeJung, Lee <irr2000@paran.com>
2021-11-19 01:25:41 +0900

    feat: text deco(line through) when task Done

--- frontend/src/components/TodoList/TodoListItem.vue
+++ frontend/src/components/TodoList/TodoListItem.vue
@@ -1,22 +1,48 @@
 <template>
   <div class="container">
-    <div class="card" :style="{'background-color': todo.colorCode}">
+    <div class="card" :style="{ 'background-color': todo.colorCode }">
       <div class="card-body">
         <div class="d-flex justify-content-between">
           <div>
-            <p class="todotitle" :style="{ 'color': fontColor }">{{ todo.taskName }}</p>
-            <p class="todoteam" :style="{ 'color': fontColor }" v-if="todo.teamId === -1">개인 todo</p>
-            <p class="todoteam" :style="{ 'color': fontColor }" v-else>{{ todo.teamName }}</p>
+            <p
+              v-if="taskDone"
+              class="todotitle"
+              :style="{ color: fontColor }"
+              style="text-decoration: line-through"
+            >
+              {{ todo.taskName }}
+            </p>
+            <p v-else class="todotitle" :style="{ color: fontColor }">
+              {{ todo.taskName }}
+            </p>
+            <p
+              class="todoteam"
+              :style="{ color: fontColor }"
+              v-if="todo.teamId === -1"
+            >
+              개인 todo
+            </p>
+            <p class="todoteam" :style="{ color: fontColor }" v-else>
+              {{ todo.teamName }}
+            </p>
           </div>
           <div style="text-align: center;">
-            <i v-if="taskDone === false" :style="{ 'color': fontColor }" class="far fa-square checkbox" @click="checkTodo()"></i>
-            <i v-else :style="{ 'color': fontColor }" class="far fa-check-square checkbox" @click="checkTodo()"></i>
-            <br>
+            <i
+              v-if="taskDone === false"
+              :style="{ color: fontColor }"
+              class="far fa-square checkbox"
+              @click="checkTodo()"
+            ></i>
+            <i
+              v-else
+              :style="{ color: fontColor }"
+              class="far fa-check-square checkbox"
+              @click="checkTodo()"
+            ></i>
+            <br />
             <i class="far fa-times-circle checkbox" @click="deleteTodo()"></i>
           </div>
-
         </div>
-
       </div>
     </div>
   </div>
@@ -26,49 +52,56 @@
 export default {
   name: "TodoListItem",
   props: {
-    todo: Object
+    todo: Object,
   },
-  data: function () {
+  data: function() {
     return {
       fontColor: "black",
-      taskDone: false
-    }
+      taskDone: false,
+    };
   },
-  created () {
-    const c = this.todo.colorCode.substring(1)      // 색상 앞의 # 제거
-    const rgb = parseInt(c, 16)   // rrggbb를 10진수로 변환
-    const r = (rgb >> 16) & 0xff  // red 추출
-    const g = (rgb >>  8) & 0xff  // green 추출
-    const b = (rgb >>  0) & 0xff  // blue 추출
-    const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b // per ITU-R BT.709
+  created() {
+    const c = this.todo.colorCode.substring(1); // 색상 앞의 # 제거
+    const rgb = parseInt(c, 16); // rrggbb를 10진수로 변환
+    const r = (rgb >> 16) & 0xff; // red 추출
+    const g = (rgb >> 8) & 0xff; // green 추출
+    const b = (rgb >> 0) & 0xff; // blue 추출
+    const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
     // 색상 선택
     if (luma < 127.5) {
-      this.fontColor = "white"
+      this.fontColor = "white";
     } else {
-      this.fontColor = "black"
+      this.fontColor = "black";
     }
     if (this.todo.teamId == -1 && this.todo.taskDone === "N") {
-      this.taskDone = false
+      this.taskDone = false;
     } else if (this.todo.teamId == -1 && this.todo.taskDone === "Y") {
-      this.taskDone = true
-    } else if (this.todo.done.includes(this.$store.state.login.userinfo.username)) {
-      this.taskDone = true
+      this.taskDone = true;
+    } else if (
+      this.todo.done.includes(this.$store.state.login.userinfo.username)
+    ) {
+      this.taskDone = true;
     } else {
-      this.taskDone = false
+      this.taskDone = false;
     }
   },
   methods: {
-    checkTodo () {
-      this.taskDone = !this.taskDone
-      this.$store.dispatch("todo/checkTodo", { "taskId":this.todo.taskId, "token": this.$store.state.login.userinfo.userToken })
+    checkTodo() {
+      this.taskDone = !this.taskDone;
+      this.$store.dispatch("todo/checkTodo", {
+        taskId: this.todo.taskId,
+        token: this.$store.state.login.userinfo.userToken,
+      });
     },
-    deleteTodo () {
-      this.$store.dispatch("todo/deleteTodo", { "taskId":this.todo.taskId, "token": this.$store.state.login.userinfo.userToken })
-      location.reload()
-    }
-  }
-
-}
+    deleteTodo() {
+      this.$store.dispatch("todo/deleteTodo", {
+        taskId: this.todo.taskId,
+        token: this.$store.state.login.userinfo.userToken,
+      });
+      location.reload();
+    },
+  },
+};
 </script>
 
 <style scoped>
