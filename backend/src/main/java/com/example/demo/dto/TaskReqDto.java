699cbc21f06afb5711e9d43786670bc9e8a65f91
HeeJung, Lee <irr2000@paran.com>
2021-11-18 11:39:15 +0900

    feat : todo bug fix

--- backend/src/main/java/com/example/demo/dto/TaskReqDto.java
+++ backend/src/main/java/com/example/demo/dto/TaskReqDto.java
@@ -2,9 +2,11 @@ package com.example.demo.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class TaskReqDto {
 
     private String taskName;
