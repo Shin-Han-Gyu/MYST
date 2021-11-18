d6343f083257ff73329423a5cf123fe43b0b60b4
leeyeeun <maple1480@pusan.ac.kr>
2021-11-18 20:15:56 +0900

    feat: 마이페이지

--- frontend/src/views/MyPage.vue
+++ frontend/src/views/MyPage.vue
@@ -1,19 +1,39 @@
 <template>
-  <div
-    v-for="myteam in this.myteams"
-    :key="myteam.id"
-  >
-    <p>{{ teamcolor }}</p>
-    <input type="color" v-model="color">
-    <button @click="changecolor()">123</button>
+  <div class = "container d-flex flex-column align-items-center">
+    <div class="w-100">
+      <h2 style="color: white; text-align: center;">
+        {{this.username}}님의 마이페이지
+      </h2>
+      <button class="btn btn-secondary" style="float: right;" >회원정보 수정</button>
+
+    </div>
+    
+
+    <h4 style="color: grey;">가입한 팀 리스트</h4>
+    <MyPageTeams
+      v-for="myteam in $store.state.myteam.myteamlist"
+      :key="myteam.id"
+      :myteam="myteam"
+      class=""
+    />
+
   </div>
+  
+  
 </template>
 
 <script>
+import MyPageTeams from "@/components/MyPage/MyPageTeams.vue";
+
 export default {
   name: "MyPage",
+  components: {
+    MyPageTeams,
+  },
   data: function() {
     return {
+      username: this.$store.state.login.userinfo.username,
+      token: null,
       color : null,
       myteams: [{id: 1, teamname: "a", teamcolor: null}]
     }
@@ -24,7 +44,17 @@ export default {
       console.log(this.myteams.teamcolor)
       console.log(this.color)
     }
-  }
+  },
+  created() {
+    this.$store.dispatch(
+      "myteam/getTeamList",
+      this.$store.state.login.userinfo.userToken
+    );
+    this.$store.dispatch(
+      "myteam/getTeamColor",
+      this.$store.state.login.userinfo.userToken
+    );
+  },
 }
 </script>
 
